f0c1269500d3f54a2ed20c8066c803a64445f8c8
tyan <tyan@rengabim.com>
2016-10-30 15:17:38 +0300

    - copyright header added to stdafx.h/cpp - plugin description file added

--- ModelExplorerPlugin/stdafx.cpp
+++ ModelExplorerPlugin/stdafx.cpp
@@ -1,1 +1,9 @@
+//
+// Copyright “Renga Software” LLC, 2016. All rights reserved.
+//
+// “Renga Software” LLC PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// “Renga Software” LLC  DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+
 #include "stdafx.h"
