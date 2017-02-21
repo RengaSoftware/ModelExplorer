e60c61e73c77480d11ab5544b98e0fddd9634b97
kohanov <alexkohspb@gmail.com>
2017-02-21 16:37:37 +0300

    - double formatting fixed - rounding changed to the nearest number

--- QtPropertyBrowser/qtpropertycommon.cpp
+++ QtPropertyBrowser/qtpropertycommon.cpp
@@ -2,21 +2,25 @@
 
 QString formatDoubleToString(const double value, const bool allowScientific, const uint precision)
 {
-	QString number = QString::number(value, allowScientific ? 'g' : 'f', precision); 
-	const bool isFractional = (abs(value - floor(value)) > 0);
+  QString number = QString::number(value, allowScientific ? 'g' : 'f', precision);
+  double rounded = static_cast<int>(value + 0.5);
+  const bool isFractional = (abs(value - rounded) > 0);
 
-	if (isFractional)
-	{
-		while (number.at(number.length() - 1) == QChar('0'))
-			number.remove(number.length() - 1, 1);
+  if (isFractional)
+  {
+    while (number.at(number.length() - 1) == QChar('0'))
+      number.remove(number.length() - 1, 1);
 
-        if (number.at(number.length() - 1) == QChar('.'))
-            number.remove(number.length() - 1, 1);
-	}
-	else
-	{
-		number = QString::number(value, allowScientific ? 'g' : 'f', allowScientific ? precision : 0);
-	}
-    
-    return number;
+    if (number.at(number.length() - 1) == QChar('.'))
+      number.remove(number.length() - 1, 1);
+
+    if (number.toDouble() == 0.0)
+      number = QString("0");
+  }
+  else
+  {
+    number = QString::number(value, allowScientific ? 'g' : 'f', allowScientific ? precision : 0);
+  }
+
+  return number;
 }
