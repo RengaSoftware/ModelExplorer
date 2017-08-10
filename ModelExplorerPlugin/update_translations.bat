f409357d9f09be5f4c18414e2ba57f3e5075d5c2
tyan <tyan@rengabim.com>
2017-08-10 10:31:14 +0300

    - update_translations.bat removed, corresponding build steps also

--- ModelExplorerPlugin/update_translations.bat
+++
@@ -1,5 +0,0 @@
-%qt5_path%\bin\lupdate.exe ".\\" -ts modelexplorerplugin_ru.ts
-%qt5_path%\bin\lupdate.exe ".\\" -ts modelexplorerplugin_en.ts
-
-%qt5_path%\bin\lrelease.exe modelexplorerplugin_ru.ts
-%qt5_path%\bin\lrelease.exe modelexplorerplugin_en.ts
