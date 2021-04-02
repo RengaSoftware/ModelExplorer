892f400d7fb58d282ad34089c20dcaf91c3570df
Evgeniy Tyan <tyan.evgeniy@gmail.com>
2021-04-02 17:17:11 +0300

    Bugfix: use access lambdas in property view builder instead of saved pointers
    Motivation: using lambda can avoid dangling references

--- ModelExplorerPlugin/RengaModelUtils.h
+++ ModelExplorerPlugin/RengaModelUtils.h
@@ -4,3 +4,7 @@
 
 
 Renga::IEntityCollectionPtr getProjectEntityCollection(Renga::IProjectPtr pProject, GUID entityType);
+
+using PropertyContainerAccess = std::function<Renga::IPropertyContainerPtr()>;
+using ParameterContainerAccess = std::function<Renga::IParameterContainerPtr()>;
+using QuantityContainerAccess = std::function<Renga::IQuantityContainerPtr()>;
